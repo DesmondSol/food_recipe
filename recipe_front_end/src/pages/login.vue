f6d38894712147591c5f6e0cd2198f7156cb58d8
Sol Tig <soltig66@gmail.com>
2022-08-20 18:25:25 +0000

    kinda working

---
+++ recipe_front_end/src/pages/login.vue
@@ -0,0 +1,23 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorlds from './components/sign_in_form.vue'
+
+import { createRouter } from 'vue-router'
+import routes from '~pages'
+
+const router = createRouter({
+  
+  routes,
+})
+
+</script>
+
+<template>
+  
+  <HelloWorlds/>
+</template>
+
+<style >
+
+</style>
