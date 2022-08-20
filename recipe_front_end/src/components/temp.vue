d4f18a1a0cb395247bef29e633d8e0ec1554d514
Sol Tig <soltig66@gmail.com>
2022-08-20 17:59:37 +0000

    every dependency added

---
+++ recipe_front_end/src/components/temp.vue
@@ -0,0 +1,16 @@
+
+<script setup>
+import { createRouter } from 'vue-router'
+import routes from '~pages'
+
+const router = createRouter({
+  // ...
+  routes,
+})
+</script>
+
+<template>
+
+</template>
+
+<style></style>
